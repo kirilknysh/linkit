2e73717866150bfd9dc94fb3c53f32c65b6f75fc
Kiril Knysh <Kiril_Knysh@epam.com>
2014-08-23 16:24:30 +0300

    Print version info

--- js/game.js
+++ js/game.js
@@ -5,11 +5,25 @@ define(["lodash", "backbone", "jquery", "js/views/header"],
         };
 
         _.extend(game, {
+            VERSION: "0.1",
+
             initialize: function (router) {
                 var header = new HeaderView({ el: $("#header").get(0) });
 
                 header.render();
                 this.eventsBus.on("header.toggleInstructions", _.bind(header.toggleInstructions, header));
+
+                this.printVersionInfo();
+            },
+
+            printVersionInfo: function () {
+                var log = console && console.log || function () {};
+                log.call(console, "===========================");
+                log.call(console, "= Backbone: " + Backbone.VERSION);
+                log.call(console, "= Lodash: " + _.VERSION);
+                log.call(console, "= jQuery: " + $.fn.jquery);
+                log.call(console, "= Link It: " + this.VERSION);
+                log.call(console, "===========================");
             }
         });
 
