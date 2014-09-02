565eedf0dff8a5124d007b14a0f64d8972528eb8
Kiril Knysh <Kiril_Knysh@epam.com>
2014-09-03 00:00:22 +0300

    Improve Old Browser error view;

--- js/views/oldBrowserError.js
+++ js/views/oldBrowserError.js
@@ -4,7 +4,19 @@ define(["lodash", "backbone", "js/views/base", "text!html/oldBrowserError.html"]
 
             template: _.template(OldBrowserErrorTemplate),
 
-            name: "old-browser-error"
+            name: "old-browser-error",
+
+            events: {
+                "click .browser-thumb": "goToBrowser"
+            },
+
+            goToBrowser: function (e) {
+                var target = e.target;
+
+                if (target && target.dataset && target.dataset.destination) {
+                    window.location.href = target.dataset.destination;
+                }
+            }
 
         });
 });
