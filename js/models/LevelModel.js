ddbd40de75ac618527119b080c458a8ebb6ae068
unknown <Kiril_Knysh@epam.com>
2014-10-08 17:42:46 +0300

    Add with/margin styling for targets/bases;

--- js/models/LevelModel.js
+++ js/models/LevelModel.js
@@ -1,7 +1,9 @@
 define(["lodash", "backbone", "jquery", "js/utils", "cryptojs-aes"],
     function (_, Backbone, $, Utils, CryptoJS) {
 
-        var LEVELS_KEY_PHRASE = "encode Level$ secr3t";
+        var LEVELS_KEY_PHRASE = "encode Level$ secr3t",
+            DEFAULT_MARGIN_LEFT = 1, DEFAULT_MARGIN_RIGHT = 1,
+            STYLE_PATTERN = "margin-left: {marginLeft}; margin-right: {marginRight}; width: {width};";
 
         return Backbone.Model.extend({
 
@@ -17,6 +19,7 @@ define(["lodash", "backbone", "jquery", "js/utils", "cryptojs-aes"],
                 this.set("basis", level.basis);
                 this.set("targets", level.targets);
                 this.set("solution", CryptoJS.AES.decrypt(level.solution, LEVELS_KEY_PHRASE).toString(CryptoJS.enc.Utf8));
+                this.setStyles(level.basis.length, level.targets.length);
 
                 return this;
             },
@@ -25,6 +28,26 @@ define(["lodash", "backbone", "jquery", "js/utils", "cryptojs-aes"],
                 var solutionStr = Utils.levels.stringifySolution(solution);
 
                 return this.get("solution") === solutionStr;
+            },
+
+            setStyles: function (basisCount, targetsCount) {
+                var baseWidth = this._getWidth(basisCount, DEFAULT_MARGIN_LEFT, DEFAULT_MARGIN_RIGHT),
+                    targerWidth = this._getWidth(targetsCount, DEFAULT_MARGIN_LEFT, DEFAULT_MARGIN_RIGHT),
+                    marginLeft = DEFAULT_MARGIN_LEFT + "%", marginRight = DEFAULT_MARGIN_RIGHT + "%",
+                    baseWidthPercent = baseWidth + "%", targetWidthPErcent = targerWidth + "%";
+
+                _.forEach(this.get("basis"), function (base) {
+                    base.style = STYLE_PATTERN.replace("{marginLeft}", marginLeft).replace("{marginRight}", marginRight)
+                        .replace("{width}", baseWidthPercent);
+                });
+                _.forEach(this.get("targets"), function (base) {
+                    base.style = STYLE_PATTERN.replace("{marginLeft}", marginLeft).replace("{marginRight}", marginRight)
+                        .replace("{width}", baseWidthPercent);
+                });
+            },
+
+            _getWidth: function (count, leftMargin, rightMargin) {
+                return (100 - (count * (leftMargin + rightMargin))) / count;
             }
 
         });
